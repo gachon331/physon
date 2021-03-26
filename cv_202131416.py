3acb8c317ab1ac2496eac4d5ad24c061092b8580
lee je wook <80080537+gachon331@users.noreply.github.com>
2021-03-26 18:16:41 +0900

    Update cv_202131416.py

--- cv_202131416.py
+++ cv_202131416.py
@@ -1,21 +1,5 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Sun Mar 14 08:20:42 2021
 
-https://opencv-python.readthedocs.io/en/latest/doc/01.imageStart/imageStart.html
-
-@author: DeskTop
-"""
-
-"""
-homework: resize all imgs in the directory with new (w, h)
-make a function with default arguments (w = 300, h = 300)
-save each file with suffix (fn_resized.jpg)
-make a function to check all new images in the directory 
-(with timedelay = default argument:2000)
-hint: import glob # imgs = glob.glob("*.jpg")
-
-"""
 
 import cv2
 
